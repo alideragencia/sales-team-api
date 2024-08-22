111c1eed5dcc1973311a0e690bdc8d76782b0e21
Lucas Schubert <lucas.eduardo.schubert@gmail.com>
2024-08-22 10:34:42 -0300

    fix: .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,17 +6,17 @@ dotenv.config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`
 import { z } from 'zod';
 
 const envSchema = z.object({
-  FIREBASE_API_KEY: z.string().min(1),
-  FIREBASE_AUTH_DOMAIN: z.string().min(1),
-  FIREBASE_DATABASE_URL: z.string().min(1),
-  FIREBASE_PROJECT_ID: z.string().min(1),
-  FIREBASE_STORAGE_BUCKET: z.string().min(1),
-  FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
-  FIREBASE_APP_ID: z.string().min(1),
-  FIREBASE_MEASUREMENT_ID: z.string().min(1),
+  // FIREBASE_API_KEY: z.string().min(1),
+  // FIREBASE_AUTH_DOMAIN: z.string().min(1),
+  // FIREBASE_DATABASE_URL: z.string().min(1),
+  // FIREBASE_PROJECT_ID: z.string().min(1),
+  // FIREBASE_STORAGE_BUCKET: z.string().min(1),
+  // FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
+  // FIREBASE_APP_ID: z.string().min(1),
+  // FIREBASE_MEASUREMENT_ID: z.string().min(1),
 
-  REDRIVE_OWNER_ID: z.string().min(1),
-  REDRIVE_TOKEN: z.string().min(1),
+  // REDRIVE_OWNER_ID: z.string().min(1),
+  // REDRIVE_TOKEN: z.string().min(1),
 
   PORT: z.coerce.number().default(8080)
 });
